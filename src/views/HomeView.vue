c58448b3be8a5eb6c9d25a0db252c7222ac5422d
MIS <amokraneakram54@gmail.com>
2023-08-29 14:10:52 +0100

    resp 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 	<main
-		class="w-full sm:w-1/2 md:w-1/4 flex flex-col gap-8 justify-start items-center bg-white/40 dark:bg-white/40 rounded-2xl drop-shadow-2xl p-4">
+		class="w-full sm:w-1/2 md:w-1/4 h-full sm:h-auto flex flex-col gap-8 justify-start items-center bg-white/40 dark:bg-white/40 rounded-2xl drop-shadow-2xl p-4">
 		<div class="fixed px-4 w-full z-40">
 			<SearchItem />
 		</div>
